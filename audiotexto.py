24e75804682b93542ddfc10293dd536695b53281
Leandro Hurtado Salazar <42324995+LeandroHurtado711@users.noreply.github.com>
2019-05-26 16:58:11 -0500

    Lectura audio->texto. No me sirve el micro :(

---
+++ audiotexto.py
@@ -0,0 +1,19 @@
+import speech_recognition as sr
+from reswords import *
+
+r = sr.Recognizer()
+
+audio = 'audio.wav'
+
+with sr.AudioFile(audio) as source:
+    audio = r.record(source)
+    print ('Done!')
+
+try:
+    text = r.recognize_google(audio)
+    print (text)
+    
+except Exception as e:
+    print (e)
+
+identificar(text)
