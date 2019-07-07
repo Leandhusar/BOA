07276edd6bc2dc6ae6f83273ac0469aa66f3b242
Leandro Hurtado Salazar <42324995+LeandroHurtado711@users.noreply.github.com>
2019-07-06 20:21:36 -0500

    Funcion de busquedas abiertas

--- app.py
+++ app.py
@@ -1,6 +1,44 @@
 from voicerec import speak
-import voiceprocessing
-import online
+from voiceprocessing import *
+from online import *
 
+"""
 texto = speak()
 print(texto)
+"""
+
+"""
+accion [0]
+objeto [1]
+print(filtrarSolicitud('boa quiero leer cincuenta sombras de gray'))
+"""
+
+#Agregar botones de entrada para solicitudes generales
+#funcionalidad de el BOTON ABURRIDO
+#... ... ...
+def botonAburrido():
+    texto = speak()
+    componentes = identificar(texto)
+    print(componentes)
+
+    #Verificar si se va a ver un video
+    if acciones[componentes[0]] == 1 and objetivos[componentes[1]] == 1:
+        buscarImagen()
+    elif acciones[componentes[0]] == 1 and objetivos[componentes[1]] == 2:
+        buscarVideo()
+    elif (acciones[componentes[0]] == 1 and objetivos[componentes[1]] == 4 or 
+        acciones[componentes[0]] == 3 and objetivos[componentes[1]] == 4):
+        buscarLibro()
+    elif acciones[componentes[0]] == 2 and objetivos[componentes[1]] == 3:
+        buscarMusica()
+    elif (acciones[componentes[0]] == 1 and objetivos[componentes[1]] == 5 or 
+        acciones[componentes[0]] == 3 and objetivos[componentes[1]] == 5):
+        buscarArticulo()
+    elif acciones[componentes[0]] == 5 and objetivos[componentes[1]] == 7:
+        buscarJuego()
+    elif (acciones[componentes[0]] == 1 and objetivos[componentes[1]] == 6 or 
+        acciones[componentes[0]] == 3 and objetivos[componentes[1]] == 6 or 
+        acciones[componentes[0]] == 4 and objetivos[componentes[1]] == 6):
+        buscarNoticia()
+    else:
+        BAF()
