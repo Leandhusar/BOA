00cbc6f42bdd64d4bd15a53a36a70ef56d426458
Leandro Hurtado Salazar <42324995+LeandroHurtado711@users.noreply.github.com>
2019-07-07 15:11:06 -0500

    Seleccion de actividades cuando no sepa qué hacer

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
 from voicerec import speak
 from voiceprocessing import *
 from online import *
+from random import choice
 
 """
 texto = speak()
 print(texto)
 """
+
+seleccion_aleatoria = [[1, 1], [1, 2], [1, 4], [3, 4], [2, 3], [1, 5], [3, 5], [5, 7], [1, 6], [3, 6], [4, 6]]
 
 #Agregar botones de entrada para solicitudes generales
 #funcionalidad de el BOTON ABURRIDO
@@ -15,14 +18,17 @@ def botonAburrido():
     componentes = identificar(texto)
     print(componentes)
 
-    #Verificar si se va a ver un video
+    #Verificar si se va a ver imagenes
     if acciones[componentes[0]] == 1 and objetivos[componentes[1]] == 1:
         buscarImagen()
+    #Verificar si se va a ver un video
     elif acciones[componentes[0]] == 1 and objetivos[componentes[1]] == 2:
         buscarVideo()
+    #verificar si se buscaran libros
     elif (acciones[componentes[0]] == 1 and objetivos[componentes[1]] == 4 or 
         acciones[componentes[0]] == 3 and objetivos[componentes[1]] == 4):
         buscarLibro()
+    #Verificar so
     elif acciones[componentes[0]] == 2 and objetivos[componentes[1]] == 3:
         buscarMusica()
     elif (acciones[componentes[0]] == 1 and objetivos[componentes[1]] == 5 or 
@@ -35,8 +41,9 @@ def botonAburrido():
         acciones[componentes[0]] == 4 and objetivos[componentes[1]] == 6):
         buscarNoticia()
     else:
+        valores = choice(seleccion_aleatoria)
         BAF()
-
+    
 def botonConcreto():
     texto = speak()
     componentes = identificar(texto)
@@ -58,6 +65,5 @@ def botonConcreto():
         juegoConcreto(componentes[1])
     else:
         print('No entendí')
-
 
 botonAburrido()
