4804621209ab0fb2ffb08638de366ecbf324fb33
Leandro Hurtado Salazar <42324995+LeandroHurtado711@users.noreply.github.com>
2019-07-07 15:19:11 -0500

    Actividad aleatoria -> No sabe que hacer

--- app.py
+++ app.py
@@ -42,14 +42,15 @@ def botonAburrido():
         buscarNoticia()
     else:
         valores = choice(seleccion_aleatoria)
-        BAF()
-    
+        BAF(valores[0], valores[1])
+
+#Busquedas especificas  
 def botonConcreto():
     texto = speak()
     componentes = identificar(texto)
     print(componentes)
 
-    #Busqueda de imagen
+    #Las busquedas de imagenes es mas restringida
     if acciones[componentes[0]] == 1 and 'imagen' in componentes[1]:
         imagenConcreta(componentes[1])
     elif acciones[componentes[0]] == 1 or 'video' in componentes[1]:
@@ -65,5 +66,28 @@ def botonConcreto():
         juegoConcreto(componentes[1])
     else:
         print('No entendí')
+
+def BAF(val_accion, val_objetivo):
+    #Selecciona las actividades por ... ruleta?
+    #Verificar si se va a ver imagenes
+    if acciones[val_accion] == 1 and objetivos[val_objetivo] == 1:
+        buscarImagen()
+    #Verificar si se va a ver un video
+    elif acciones[val_accion] == 1 and objetivos[val_objetivo] == 2:
+        buscarVideo()
+    #verificar si se buscaran libros
+    elif (acciones[val_accion] == 1 and objetivos[val_objetivo] == 4 or 
+        acciones[val_accion] == 3 and objetivos[val_objetivo] == 4):
+        buscarLibro()
+    #Verificar so
+    elif acciones[val_accion] == 2 and objetivos[val_objetivo] == 3:
+        buscarMusica()
+    elif (acciones[val_accion] == 1 and objetivos[val_objetivo] == 5 or 
+        acciones[val_accion] == 3 and objetivos[val_objetivo] == 5):
+        buscarArticulo()
+    elif acciones[val_accion] == 5 and objetivos[val_objetivo] == 7:
+        buscarJuego()
+    else:
+        buscarNoticia()
 
 botonAburrido()
