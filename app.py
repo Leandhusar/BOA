d1f690fa50bb60bdc9c521196118729b385a31e8
Leandro Hurtado Salazar <42324995+LeandroHurtado711@users.noreply.github.com>
2019-07-08 21:02:52 -0500

    Agrega boton de ayuda y mejoras en GUI

--- app.py
+++ app.py
@@ -18,6 +18,7 @@ def botonAburrido():
     texto = speak()
     componentes = identificar(texto)
     print(componentes)
+    seleccion_aleatoria.append([componentes[0],componentes[1]])
 
     if componentes[0] == None or componentes[1] == None:
         valores = choice(seleccion_aleatoria)
@@ -57,7 +58,7 @@ def botonConcreto():
     #Las busquedas de imagenes es mas restringida
     if acciones[componentes[0]] == 1 and 'imagen' in componentes[1]:
         imagenConcreta(componentes[1])
-    elif acciones[componentes[0]] == 1 and 'video' in componentes[1]:
+    elif acciones[componentes[0]] == 1 or 'video' in componentes[1]:
         videoConcreto(componentes[1])
     elif (acciones[componentes[0]] == 1 and 'libro' in componentes[1] or 
         acciones[componentes[0]] == 3):
@@ -95,25 +96,45 @@ def BAF(val_accion, val_objetivo):
         buscarNoticia()
 
 #- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+def ayuda():
+    print('este es el boton de ayuda')
+    vayuda = Tk()
+    vayuda.config(bg = '#505050')
+    
+    texto = 'Bienvenido a BOA \n \n Si quieres hacer algo pero no sabes qué, presiona el "botón aburrido" \n \n Si quieres hacer algo concreto presiona el boton "¿En qué te puedo ayudar?" \n'
+    label = Label(vayuda, text = texto)
+    acs = str(seleccion_aleatoria)
+    labelacs = Label(vayuda, text = acs)
+    label.config(bg = '#505050', fg = '#ffffff')
+    labelacs.config(bg = '#505050', fg = '#ffffff')
+    label.pack()
+    labelacs.pack()
+
+
 #GUI
 ventana = Tk()
-ventana.config(bg='#5f5f5f')
+ventana.config(bg = '#5f5f5f')
 ventana.geometry('150x210')
 ventana.resizable(width = False, height = False)
 
+icono = PhotoImage(file = "imagen.png")
+
 # Enlezamos la función a la acción del botón
+bayuda = Button(ventana, command = ayuda)
 baburrido = Button(ventana, text = "Botón aburrido", command = botonAburrido)
 bconcreto = Button(ventana, text = "¿En qué puedo ayudarte?", command = botonConcreto)
 
-baburrido.config(bg = '#60b0f4')
-bconcreto.config(bg = '#60b0f4')
+bayuda.config(image = icono)
+baburrido.config(bg = '#60b0f4', width = 30)
+bconcreto.config(bg = '#60b0f4', width = 30, height = 30)
 
 
-image = PhotoImage(file = "imagen.png")
-label = Label(image = image)
-label.pack()
+#image = PhotoImage(file = "imagen.png")
+#label = Label(image = image)
+#label.pack()
 
-
+bayuda.pack()
 baburrido.pack()
 bconcreto.pack()
 
